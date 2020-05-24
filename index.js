51aad6ea1ae1260be56ee6b9b52b004997654682
Rosie Choi <rosiechoimanso@gmail.com>
2020-05-24 16:54:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Code your solution here!
 function printString(myString) {
- let newString;
+  console.log(myString[myString.length - 1]);
+ 
   if (myString.length > 1) {
-   for (var i = myString.length - 1; i >= 0; i--) {
-        newString += myString[i];
-    }
-    printString(newString)
+    let mySubString = myString.substring(myString.length - 1, myString.length);
+    printString(mySubString);
   } else {
     return true;
   }
