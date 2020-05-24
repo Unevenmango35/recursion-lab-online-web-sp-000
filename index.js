421eac452bafe797ad461408a41a9934828fe03c
Rosie Choi <rosiechoimanso@gmail.com>
2020-05-24 16:46:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution here!
-
+function printString(myString) {
+ let newString;
+  if (myString.length > 1) {
+   for (var i = myString.length - 1; i >= 0; i--) {
+        newString += myString[i];
+    }
+    printString(newString)
+  } else {
+    return true;
+  }
+}
